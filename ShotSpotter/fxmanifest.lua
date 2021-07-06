36834016d0ca73a0ed207a3cbf71f7b0fc0dad21
ItzEndah <79053058+ItzEndah@users.noreply.github.com>
2021-07-06 20:43:37 +0200

    Add files via upload

---
+++ ShotSpotter/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'ShotSpotter'
+description 'Gunshot detection and alert system.'
+author 'ItzEndah'
+version '1.0.3'
+
+server_script "server.lua"
+client_script {
+	"client.lua",
+	"config.lua"
+}
