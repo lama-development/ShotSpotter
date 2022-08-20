c6efb02651dae2f2680815364e7b2419abdac058
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-08-20 17:13:14 +0200

    Removed version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,11 @@
-fx_version 'bodacious'
+fx_version 'cerulean'
 game 'gta5'
 
 name 'ShotSpotter'
-description 'Gunshot detection and alert system.'
+description 'Gunshot detection and alert system'
 author 'Lama'
 version '1.4'
 
-server_script {
-	'server.lua',
-	'versioncheck.lua'
-}
-
-client_script {
-	'client.lua',
-	'config.lua'
-}
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua'
