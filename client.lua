c23d1cb9b50e5ba8ca6fc255070625eb76fbba82
Mehdi <57235093+Mehdi-MMO@users.noreply.github.com>
2023-02-09 19:51:03 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -7,9 +7,7 @@ Do not edit below if you don't know what you are doing
 -- Notify player
 RegisterNetEvent('gunshotNotify')
 AddEventHandler('gunshotNotify', function(alert)
-    if Config.InstantNotify then
-        wait(Config.NotifyTime)
-    end
+    wait(Config.NotifyTime)
     if IsPedInAnyPoliceVehicle(PlayerPedId()) then
         PlaySoundFrontend(-1, "TIMER_STOP", "HUD_MINI_GAME_SOUNDSET", 1)
         Notify(alert)
